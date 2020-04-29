c77e3b2fd28d5ca0ffa8e97c1b30c6c95a817760
Amuri Bonface <amuribonface@gmail.com>
2020-04-29 17:47:58 +0300

    Update Constants.java

--- app/src/main/java/org/nearbyshops/enduserappnew/Checkout/mobile_pay/Constants.java
+++ app/src/main/java/org/nearbyshops/enduserappnew/Checkout/mobile_pay/Constants.java
@@ -2,6 +2,6 @@ package org.nearbyshops.enduserappnew.Checkout.mobile_pay;
 
 public class Constants {
 	//Mpesa call back requiests are directed 
-    public static final String CALLBACKURL="http://35.223.171.76/mpesa";
-    public static final String PAYCONFIGURL="http://35.223.171.76/servlet/mpesa_config.php";
+    public static final String CALLBACKURL="http://";
+    public static final String PAYCONFIGURL="http://";
 }
